7db4af6bc9e89de88d83962c7321b24786491f29
RadoProgramer <145141434+RadoProgramer@users.noreply.github.com>
2024-02-05 09:43:51 +0000

    Update 01.mjs

---
+++ src/scripts/01.mjs
@@ -0,0 +1,59 @@
+"use strict";
+
+// let year = 2024;
+// console.log(`Rok przed zmianą: ${year}`);
+// year = 2020;
+// console.log(`Rok po zmianie: ${year}`);
+
+// const birthYear = 1999;
+// console.log(`Rok przed zmianą: ${birthYear}`);
+// birthYear = 2003;
+// console.log(`Rok po zmianie: ${birthYear}`);
+
+// Właściwość dla ciągu znaków "JavaScript is fun!" przechowująca jego długość
+// console.log("JavaScript is fun!".length);
+
+// number
+// const theVariable = 1;
+// const theOtherVariable = 1.45;
+
+// string
+// const DEFAULT_THEME = "light";
+
+// boolean
+// const IS_DEFAULT_THEME_LIGHT = true;
+
+// null
+// const currentTheme = null;
+
+// undefined
+// const nonExistent = undefined;
+
+// const zero = 0;
+
+// console.log(typeof zero);
+
+// let num = 0;
+
+// console.log(num);
+
+// console.log(10 !== "10");
+
+// console.log(!("5" == 6) || 11 == "10");
+
+// console.log("PrzykłaD".startsWith("Pr"));
+
+// function printManyLines() {
+//     console.log("Line 01");
+//     console.log("Line 02");
+//     console.log("Line 03");
+//     console.log("Line 04");
+//     console.log("Line 05");
+//     console.log("Line 06");
+//     console.log("Line 07");
+//     console.log("Line 08");
+// }
+
+// printManyLines();
+// console.log("------przerwa------");
+// printManyLines();
