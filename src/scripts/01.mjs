837f01e2d2abb4ccacb8f17647d42d7a3fecbd41
RadoProgramer <145141434+RadoProgramer@users.noreply.github.com>
2024-02-07 15:52:09 +0000

    up

--- src/scripts/01.mjs
+++ src/scripts/01.mjs
@@ -1,4 +1,5 @@
 "use strict";
+
 // let year = 2024;
 // console.log(`Rok przed zmianą: ${year}`);
 // year = 2020;
@@ -56,3 +57,38 @@
 // printManyLines();
 // console.log("------przerwa------");
 // printManyLines();
+
+// const nan = Number("a;sdka;lskd;lkd"); // NaN
+// const num = Number("123"); // 123
+
+// console.log(Number.isNaN(num));
+
+// const result = ((0.17 * 100) + (0.24 * 100)) / 100;
+// const result = 0.17 + 0.24;
+
+// console.log(Number(result.toFixed(2))); // toFixed zmienia typ wartości na string!
+
+// console.log((Math.random() * (10 - 1) + 1));
+
+// const min = 1;
+// const max = 100;
+
+// console.log((Math.random() * (max - min) + min));
+
+// console.log("Ala ma kota".replace("Ala", "Ewa"));
+// console.log("Ala ma kota. Ala jest szczęśliwa.".replaceAll("Ala", "Ewa"));
+
+// console.log("Ala ma kota.".slice(4, 6));
+
+// console.log("Test danych wyświetlanych w konsoli przeglądarki metodą log");
+// console.info("Test danych wyświetlanych w konsoli przeglądarki metodą info");
+// console.warn("Test danych wyświetlanych w konsoli przeglądarki metodą warn");
+// console.error("Test danych wyświetlanych w konsoli przeglądarki metodą error");
+
+// window.alert("Test wyskakującego powiadomienia!");
+// const response = window.confirm("Czy na pewno?");
+// const response = window.prompt("Wprowadź swoje imię");
+
+// console.log("Odpowiedź: " + response);
+// console.log(`Odpowiedź: ${response}`);
+// console.log("Odpowiedź:", response, `(typ: ${typeof response})`);
